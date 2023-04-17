9d39aa2460a13280eb7781c8af85fde53ea962bd
Wen Xiang <wchen2654@gmail.com>
2023-04-16 21:19:55 -0500

    Reorganized repo

---
+++ Code/Function_requirements/login.js
@@ -0,0 +1,44 @@
+var mysql = require('mysql');
+
+async function Login(username, password)
+{
+
+    var con = mysql.createConnection({
+        host:"127.0.0.1",
+        user:"root",
+        password:"root",
+        database:"SELab"
+    });
+    
+    con.connect(function(err) {
+        if (err) throw err;
+    });
+
+    return new Promise((resolve, reject) => {
+
+        var query = mysql.format("SELECT * FROM SELab.users WHERE BINARY username = ? AND BINARY password = ?", [username, password]);
+        con.query(query, function(err, result) {
+            console.log(result);
+            if (err) reject(err);
+            if (!result.length)
+            {
+                resolve(false);
+            }
+            else
+            {
+                if(result[0].blocked_flag)
+                {
+                    resolve(false);
+                }
+                else
+                {
+                    resolve(true);
+                }
+            }
+        });
+
+        con.end();
+    });
+}
+
+exports.Login = Login;
---
+++ Code/Unit_Test/login.test.js
@@ -0,0 +1,21 @@
+var Login = require('../Function_requirements/login');
+
+test('Login information that exist in the database should result in true', async () => {
+    expect(await Login.Login("Bob", "asdf")).toBe(true);
+});
+
+test('Username and password exist but doesn\'t match. Should result in false', async () => {
+    expect(await Login.Login("adl409", "idk123")).toBe(false);
+});
+
+test('Username and password exist but doesn\'t match. Should result in false', async () => {
+    expect(await Login.Login("WCHEN2654", "idk123")).toBe(false);
+});
+
+test('Password exist but username doesn\'t. Should result in false', async () => {
+    expect(await Login.Login("random_username", "idk123")).toBe(false);
+});
+
+test('Login information exist in the database but is blocked. Should result in false', async () => {
+    expect(await Login.Login("CoolDude", "asdf")).toBe(false);
+});
