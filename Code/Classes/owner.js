16f7763f83b76e417328573189755e7c914323d0
Wen Xiang <wchen2654@gmail.com>
2023-04-16 22:41:39 -0500

    Fixed bugs with SetAdmin Issue #23

--- Code/Classes/owner.js
+++ Code/Classes/owner.js
@@ -46,7 +46,6 @@ const Owner = class{
 
             var query = mysql.format("UPDATE users SET type_flag = 2 WHERE user_id = ?", [user_id]);
             con.query(query, function(err, result) {
-                console.log(result);
                 if (err) reject(err);
 
                 if(result.affectedRows == 0)
@@ -67,6 +66,15 @@ const Owner = class{
     {
         return new Promise((resolve, reject) => {
 
+            var con = mysql.createConnection({
+                host:"127.0.0.1",
+                user:"root",
+                password:"root",
+                database:"SELab"
+            });
+
+            con.connect();
+
             var query = mysql.format("UPDATE SELab.users SET blocked_flag = 1 WHERE user_id = ?", [user_id]);
             con.query(query, function(err, result) {
                 if (err) reject(err);
@@ -79,6 +87,9 @@ const Owner = class{
                     resolve(true);
                 }
             });
+
+            con.end();
+
         });
     }
 
@@ -86,6 +97,15 @@ const Owner = class{
     { 
         return new Promise((resolve, reject) => {
             var mysql = require('mysql');
+
+            var con = mysql.createConnection({
+                host:"127.0.0.1",
+                user:"root",
+                password:"root",
+                database:"SELab"
+            });
+
+            con.connect();
 
             var query = mysql.format("UPDATE SELab.users SET blocked_flag = 0 WHERE user_id = ?", [user_id]);
             con.query(query, function(err, result) {
@@ -99,12 +119,24 @@ const Owner = class{
                     resolve(true);
                 }
             });
+
+            con.end();
+
         });
     }
 
     async SetSeller(user_id)
     {
         return new Promise((resolve, reject) => {
+
+            var con = mysql.createConnection({
+                host:"127.0.0.1",
+                user:"root",
+                password:"root",
+                database:"SELab"
+            });
+
+            con.connect();
 
             var query = mysql.format("UPDATE SELab.users SET type_flag = 1 WHERE user_id = ?", [user_id]);
 
@@ -120,12 +152,24 @@ const Owner = class{
                     resolve(true);
                 }
             });
+
+            con.end();
+
         });
     }
 
     async SetBuyer(user_id)
     {
         return new Promise((resolve, reject) => {
+
+            var con = mysql.createConnection({
+                host:"127.0.0.1",
+                user:"root",
+                password:"root",
+                database:"SELab"
+            });
+
+            con.connect();
 
             var query = mysql.format("UPDATE SELab.users SET type_flag = 0 WHERE user_id = ?", [user_id]);
             con.query(query, function(err, result) {
@@ -139,6 +183,9 @@ const Owner = class{
                     resolve(true);
                 }
             });
+
+            con.end();
+
         });
     }
 };
